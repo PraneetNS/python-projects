50fd57c08bdecde7e35ce761d1bc0a86e06c6085
PraneetNS <savantpraneet@gmail.com>
2025-12-21 23:49:21 +0530

    kadane max sub array

---
+++ kadane_max_sub_array.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input("Enter array: ").split()))
+
+current_sum = arr[0]
+max_sum = arr[0]
+
+for i in range(1, len(arr)):
+    current_sum = max(arr[i], current_sum + arr[i])
+    max_sum = max(max_sum, current_sum)
+
+print("Max subarray sum:", max_sum)

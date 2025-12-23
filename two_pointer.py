f2b579813cfba63fc95baccb5fda7b3b4293771f
PraneetNS <savantpraneet@gmail.com>
2025-12-23 22:59:43 +0530

    two pointer

---
+++ two_pointer.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+l, r = 0, len(arr) - 1
+max_area = 0
+
+while l < r:
+    area = min(arr[l], arr[r]) * (r - l)
+    max_area = max(max_area, area)
+
+    if arr[l] < arr[r]:
+        l += 1
+    else:
+        r -= 1
+
+print(max_area)

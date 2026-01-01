f16eaca6ffe411d2e07f5e6af457e3ba4e3cfc2c
PraneetNS <savantpraneet@gmail.com>
2026-01-01 23:52:05 +0530

    kadane's algo

---
+++ kadane's_algo.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+
+curr = arr[0]
+best = arr[0]
+
+for i in range(1, len(arr)):
+    curr = max(arr[i], curr + arr[i])
+    best = max(best, curr)
+
+print("Max subarray sum:", best)

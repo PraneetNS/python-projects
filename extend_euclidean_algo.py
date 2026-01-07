9c4115bd0cc17425f1d35636fcff705a4203ad50
PraneetNS <savantpraneet@gmail.com>
2026-01-07 23:28:24 +0530

    extended euclidean algo

---
+++ extend_euclidean_algo.py
@@ -0,0 +1,15 @@
+def extended_gcd(a, b):
+    if b == 0:
+        return a, 1, 0
+
+    gcd, x1, y1 = extended_gcd(b, a % b)
+    x = y1
+    y = x1 - (a // b) * y1
+    return gcd, x, y
+
+
+a, b = map(int, input().split())
+g, x, y = extended_gcd(a, b)
+
+print("GCD:", g)
+print("x:", x, "y:", y)

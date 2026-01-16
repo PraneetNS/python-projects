557dc8a9fdb72dae3c27772bcc9a1869cc25a70c
PraneetNS <savantpraneet@gmail.com>
2026-01-16 23:25:13 +0530

    Principle of Optimality

---
+++ principle_of_optimality.py
@@ -0,0 +1,12 @@
+coins = list(map(int, input().split()))
+amount = int(input())
+
+dp = [float('inf')] * (amount + 1)
+dp[0] = 0
+
+for i in range(1, amount + 1):
+    for c in coins:
+        if i - c >= 0:
+            dp[i] = min(dp[i], dp[i - c] + 1)
+
+print(dp[amount] if dp[amount] != float('inf') else -1)

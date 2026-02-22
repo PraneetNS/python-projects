75dc869239a6a4d749a9a199260323fd2b744670
PraneetNS <savantpraneet@gmail.com>
2026-02-22 23:53:36 +0530

    Burst Ballons

---
+++ burst_ballons.py
@@ -0,0 +1,12 @@
+nums = [1] + list(map(int, input().split())) + [1]
+n = len(nums)
+dp = [[0]*n for _ in range(n)]
+
+for length in range(2, n):
+    for l in range(n-length):
+        r = l + length
+        for k in range(l+1, r):
+            dp[l][r] = max(dp[l][r],
+                           nums[l]*nums[k]*nums[r] + dp[l][k] + dp[k][r])
+
+print(dp[0][n-1])

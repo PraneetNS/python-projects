6027254e843953613b83943913d36e83b43b912e
PraneetNS <savantpraneet@gmail.com>
2026-02-28 23:19:37 +0530

    word search

---
+++ word_search.py
@@ -0,0 +1,27 @@
+board = eval(input())
+word = input()
+
+m, n = len(board), len(board[0])
+
+def dfs(r,c,i):
+    if i == len(word):
+        return True
+    if r<0 or c<0 or r>=m or c>=n or board[r][c] != word[i]:
+        return False
+
+    temp = board[r][c]
+    board[r][c] = "#"
+
+    found = (dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or
+             dfs(r,c+1,i+1) or dfs(r,c-1,i+1))
+
+    board[r][c] = temp
+    return found
+
+for i in range(m):
+    for j in range(n):
+        if dfs(i,j,0):
+            print(True)
+            exit()
+
+print(False)

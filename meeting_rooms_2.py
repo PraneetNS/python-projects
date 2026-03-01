e6fdac48416ca5a5c5e7c5ec6aaf79c4b3190e2b
PraneetNS <savantpraneet@gmail.com>
2026-03-01 23:46:20 +0530

    meeting rooms 2

---
+++ meeting_rooms_2.py
@@ -0,0 +1,12 @@
+import heapq
+
+def minMeetingRooms(intervals):
+    intervals.sort()
+    heap = []
+    
+    for start, end in intervals:
+        if heap and heap[0] <= start:
+            heapq.heappop(heap)
+        heapq.heappush(heap, end)
+    
+    return len(heap)

fcad39a7f6877dd9a39e6a4d490f7409a8545699
PraneetNS <savantpraneet@gmail.com>
2026-03-03 23:05:25 +0530

    combination sum

---
+++ combination_sum.py
@@ -0,0 +1,17 @@
+def combinationSum(candidates, target):
+    res = []
+
+    def backtrack(i, cur, total):
+        if total == target:
+            res.append(cur.copy())
+            return
+        if i >= len(candidates) or total > target:
+            return
+        
+        cur.append(candidates[i])
+        backtrack(i, cur, total + candidates[i])
+        cur.pop()
+        backtrack(i+1, cur, total)
+
+    backtrack(0, [], 0)
+    return res
